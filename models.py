27d63ce1f39be87404cf431934196375cb6a6c2e
papupreethi <68058809+papupreethi@users.noreply.github.com>
2020-07-18 18:12:10 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Register(models.Model):
-    name = models.CharField(max_length=150)
-    mail = models.CharField(max_length=200)
-    phone = models.CharField(max_length=20)
-    psw = models.CharField(max_length=10)
-    pswr = models.CharField(max_length=10)
-
-    def __str__(self):
-        return self.name
-
 
 class Doctor(models.Model):
     name = models.CharField(max_length=50)
