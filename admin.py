b38913506fb01e7b62bdca4971c6e333c8eed937
papupreethi <68058809+papupreethi@users.noreply.github.com>
2020-07-18 14:28:22 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from.models import *
 
 # Register your models here.
-admin.site.register(Register)
 admin.site.register(Doctor)
 admin.site.register(Patient)
 admin.site.register(Appointment)
